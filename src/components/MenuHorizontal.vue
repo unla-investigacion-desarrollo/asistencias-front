e58adc485d23d3e8f2d1321f5244ddabfb2e7525
Nehuen Diaz Burcet <n.uen@hotmail.com>
2024-06-27 19:56:01 -0300

    Agrego acceso al login desde el menu horizontal

--- src/components/MenuHorizontal.vue
+++ src/components/MenuHorizontal.vue
@@ -1,5 +1,8 @@
 <template>
     <v-toolbar :elevation="8" color="blue-grey-darken-4" title="Eventos UNLa">
+        <v-btn icon>
+          <v-icon @click="login">mdi-account</v-icon>
+        </v-btn>
         <v-btn @click="inscripcion">
             <span>Inscribirse</span>
         </v-btn>
@@ -12,6 +15,9 @@ export default {
     methods: {
         inscripcion(){
             this.$router.push('inscripcion')
+        },
+        login(){
+            this.$router.push('login')
         }
     },
 }
