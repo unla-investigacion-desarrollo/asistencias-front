f53b57796f0b33a8e623c1706a9f8caa857f16b0
Nehuen Diaz Burcet <n.uen@hotmail.com>
2025-07-25 22:38:30 -0300

    Cambios de estilos en el login

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,40 +1,49 @@
 <template>
+  <v-container>
     <v-form>
-    
-    <v-text-field
-      v-model="model.email"
-      label="Email"
-      :rules="validationEmail"
-      maxlength="45"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      v-model="model.clave"
-      :counter="45"
-      label="Contraseña"
-      :rules="validationText"
-      maxlength="45"
-      required
-    ></v-text-field>
-
-    <div class="button_container">
-      <v-btn
-      @click="continuar"
-      color="primary"
-    >
-      Iniciar sesión
-      </v-btn>
-      <v-btn
-      @click="registro"
-      color="primary"
-      class="registro"
-    >
-      Registrarse
-      </v-btn>
-    </div>
-    
-  </v-form>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="model.email"
+            label="Email"
+            :rules="validationEmail"
+            maxlength="45"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="model.clave"
+            :counter="45"
+            label="Contraseña"
+            :rules="validationText"
+            maxlength="45"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row class="button_container">
+        <v-col>
+          <v-btn
+            @click="continuar"
+            color="primary"
+          >
+            Iniciar sesión
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+            @click="registro"
+            color="primary"
+          >
+            Registrarse
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
 </template>
   
 <script>
@@ -74,7 +83,4 @@ export default {
   text-align: center;
 }
 
-.registro{
-  margin-left: 5%;
-}
 </style>
